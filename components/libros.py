df4c15e7677dae81d288ab4404bf194edf5082a5
Faustino <mirandafaustino02@gmail.com>
2026-05-15 11:25:34 -0300

    Primer commit

---
+++ components/libros.py
@@ -0,0 +1,13 @@
+class Libros:
+    def __init__(self, id, titulo, autor, disponibilidad, genero, fecha_de_lanzamiento, precio, idioma, stock):
+        self.id = id
+        self.titulo = titulo
+        self.autor = autor
+        self.disponibilidad = disponibilidad
+        self.genero = genero
+        self.fecha_de_lanzamiento = fecha_de_lanzamiento
+        self.precio = precio
+        self.idioma = idioma
+        self.stock = stock
+
+
